bbfda24f43fa6c485186cf5664676a27c84b753f
sanket2000 <52121362+sanket2000@users.noreply.github.com>
2020-05-01 14:48:07 +0530

    added comments

---
+++ hardwareTesting.py
@@ -0,0 +1,23 @@
+# hardwareTesting.py
+'''
+This file communicates directly to actuating hardware for testing purpose
+'''
+
+import serial
+
+# sampeling speed
+BAUD_RATE = 115200
+
+# initializing which serial port to connect
+try:
+	print("Connected to : /dev/ttyACM0")
+	s=serial.Serial('/dev/ttyACM0',BAUD_RATE)
+except:
+	print("Connected to : /dev/ttyACM1")
+	s=serial.Serial('/dev/ttyACM1',BAUD_RATE)
+
+# reads char from user and sends to hardware control (Arduino)
+# each character is mapped to an action (See Arduino's code)
+while True:
+	ip = input('Enter char 0 to 9: ')
+	s.write(str.encode(ip))
